6abce5f04ae0ea457829d17da2356d604bfb5904
LiYu <2248868140@qq.com>
2021-08-11 17:58:15 +0800

    添加全局样式和变量

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,48 @@
 <template>
-<Layout>
-  <p>money.vue</p>
-</Layout>
+  <div>
+    <div>
+      <ul class="types">
+        <li class="selected">支出</li>
+        <li>收入</li>
+      </ul>
+    </div>
+    <div class="tags">
+      <ul>
+        <li>餐饮</li>
+        <li>购物</li>
+        <li>居住</li>
+        <li>交通</li>
+        <li>新增</li>
+      </ul>
+    </div>
+    <div>
+      <label class="notes">
+        <span class="name">备注</span>
+        <input type="text">
+      </label>
+    </div>
+    <div class="numberPad">
+      <div class="output">100</div>
+      <div class="buttons">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>删除</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>清空</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>OK</button>
+        <button>.</button>
+        <button>0</button>
+      </div>
+    </div>
+    <hr>
+    <router-link to="/labels" class="cancel">取消</router-link>
+  </div>
 </template>
 
 <script lang="ts">
@@ -11,3 +52,8 @@ export default {
 }
 </script>
 
+<style lang="scss" scoped>
+.cancel {
+  border: 1px solid black;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -11,6 +11,8 @@ module.exports = {
         .test(/\.svg$/)   //规则的特点
         .include.add(dir).end() // 这个规则只包含 icons 目录
         .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract:false}).end() //使用哪些loader extracr:false是不要把它解析出文件
+        .use('svgo-loader').loader('svgo-loader')
+        .tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]})).end() //解决了svg自带fill属性的问题
     config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
     config.module.rule('svg').exclude.add(dir) // 其他 svg loader 排除 icons 目录
   }
