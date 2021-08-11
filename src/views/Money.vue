72b672d4ca20b4a76d6eb58188f4d2da8694e78f
LiYu <2248868140@qq.com>
2021-08-11 22:11:35 +0800

    完成tags的css

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -7,13 +7,30 @@
       </ul>
     </div>
     <div class="tags">
-      <ul>
-        <li>餐饮</li>
-        <li>购物</li>
-        <li>居住</li>
-        <li>交通</li>
-        <li>新增</li>
+      <div class="content">
+      <ul class="current">
+        <li>
+          <Icon name="food"></Icon>
+          <span>餐饮</span>
+        </li>
+        <li>
+          <Icon name="shopping"></Icon>
+          <span>购物</span>
+        </li>
+        <li>
+          <Icon name="live"></Icon>
+          <span>居住</span>
+        </li>
+        <li>
+          <Icon name="traffic"></Icon>
+          <span>交通</span>
+        </li>
+        <li>
+          <Icon name="add"></Icon>
+          <span>新增</span>
+        </li>
       </ul>
+      </div>
     </div>
     <div>
       <label class="notes">
@@ -55,5 +72,33 @@ export default {
 <style lang="scss" scoped>
 .cancel {
   border: 1px solid black;
+}
+.types {
+  border: 1px solid red;
+}
+.tags {
+  border: 1px solid blue;
+  padding-left: 41.5px;
+   >.content {
+     margin-right: -35px;
+     > .current {
+       display: flex;
+       flex-wrap: wrap;
+       > li {
+         margin-right: 35px;
+         display: flex;
+         flex-direction: column;
+         align-items: center;
+         justify-content: center;
+         margin-bottom: 25px;
+         > .icon {
+           background:rgb(245, 245, 245);
+           border-radius: 50%;
+           width: 56px;
+           height: 56px;
+         }
+       }
+     }
+   }
 }
 </style>
