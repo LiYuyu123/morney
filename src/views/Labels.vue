da47b4923abc77cbf42c4d0ea69aca66b6db04f5
LiYu <2248868140@qq.com>
2021-08-11 14:53:19 +0800

    创建 <Icon />

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script lang="ts">
-
 export default {
   name: 'Labels'
 }
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,17 @@
+/*eslint-disable */
+const path=require('path')
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  chainWebpack:config=>{
+    const dir = path.resolve(__dirname, 'src/assets/icons')
+
+    config.module
+        .rule('svg-sprite') //添加一个规则
+        .test(/\.svg$/)   //规则的特点
+        .include.add(dir).end() // 这个规则只包含 icons 目录
+        .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract:false}).end() //使用哪些loader extracr:false是不要把它解析出文件
+    config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
+    config.module.rule('svg').exclude.add(dir) // 其他 svg loader 排除 icons 目录
+  }
 }
