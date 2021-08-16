eb68c0251fc7edcd8c94fd6ca89b080eac655f8b
LiYu <2248868140@qq.com>
2021-08-16 14:01:28 +0800

    完成editLabel组件的tag展示

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,17 +1,17 @@
 <template>
-<Layout>
-  <div class="navBar">
-    <Icon class="leftIcon" name="left" />
-    <span class="title">编辑</span>
-    <span class="rightIcon"></span>
-  </div>
-  <div class="form-wrapper">
-    <FormItem  field-name="标签名" placeholder="请输入标签名" />
-  </div>
-  <div class="button-wrapper">
-    <Button>删除标签</Button>
-  </div>
-</Layout>
+  <Layout>
+    <div class="navBar">
+      <Icon class="leftIcon" name="left"/>
+      <span class="title">编辑</span>
+      <span class="rightIcon"></span>
+    </div>
+    <div class="form-wrapper">
+      <FormItem :value="tag.name" field-name="标签名" placeholder="请输入标签名"/>
+    </div>
+    <div class="button-wrapper">
+      <Button>删除标签</Button>
+    </div>
+  </Layout>
 
 </template>
 
@@ -23,48 +23,57 @@ import tagListModel from '@/models/tagListModel';
 import Labels from '@/views/Labels.vue';
 import FormItem from '@/components/money/FormItem.vue';
 import Button from '@/components/Button.vue';
+
 @Component({
   components: {Button, FormItem, Labels}
 })
 export default class EditLabel extends Vue {
-  created(){
-    const id =this.$route.params.id;
-    tagListModel.fetch
-    const tags=tagListModel.data
-    const tag=tags.filter(t=>t.id===id)[0]
-    if(tag){
-      console.log(tag)
-    }else {
-      this.$router.replace('/404')
+  tag?: { id: string, name: string } = undefined;
+
+  created() {
+    const id = this.$route.params.id;
+    tagListModel.fetch;
+    const tags = tagListModel.data;
+    const tag = tags.filter(t => t.id === id)[0];
+    if (tag) {
+      this.tag = tag;
+      console.log(this.tag)
+    } else {
+      this.$router.replace('/404');
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.navBar{
+.navBar {
   padding: 7px 16px;
   font-size: 24px;
   display: flex;
   align-items: center;
   justify-content: space-between;
   background: #c4c4c4;
-  .title{
+
+  .title {
   }
-  .rightIcon{
+
+  .rightIcon {
     width: 24px;
     height: 24px;
   }
-  .leftIcon{
+
+  .leftIcon {
     width: 24px;
     height: 24px;
   }
 }
-.form-wrapper{
+
+.form-wrapper {
   background: white;
   margin-top: 8px;
 }
-.button-wrapper{
+
+.button-wrapper {
   text-align: center;
   padding: 16px;
   margin-top: 44-16px;
