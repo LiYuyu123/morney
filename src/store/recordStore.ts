524a86dea2806e7a1e173f33296f23f1792dc9ab
LiYu <2248868140@qq.com>
2021-08-17 15:40:48 +0800

    模块化的store，删除了models目录

---
+++ src/store/recordStore.ts
@@ -0,0 +1,24 @@
+import clone from '@/lib/Clone';
+const localstorageName='recordList'
+
+
+
+ const recordStore={
+    recordList:[] as RecordItem[],
+    fetchRecords(){
+        this.recordList=JSON.parse( window.localStorage.getItem(localstorageName) || '[]') as RecordItem[];
+        return this.recordList
+    },
+    saveRecords(){
+        window.localStorage.setItem('recordList', JSON.stringify(this.recordList));
+    },
+    createRecord(record: RecordItem) {
+        const record2:RecordItem =clone(record)
+        record2.createAt=new Date()
+        this.recordList && this.recordList.push(record2);
+        this.saveRecords()
+    },
+}
+recordStore.fetchRecords()
+
+export default recordStore
