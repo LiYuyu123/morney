7f2672d80933b2fb567303d593d93ede18f2008b
LiYu <2248868140@qq.com>
2021-08-18 12:47:34 +0800

    把tag改成currentTag

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -10,7 +10,7 @@
                 placeholder="请输入标金额"/>
       <FormItem field-name="日期"
                 placeholder="请输入标日期"/>
-      <FormItem :value="tag.name"
+      <FormItem :value="currentTag.name"
                 @update:value="update"
                 field-name="备注"
                 placeholder="请输入标签名"/>
@@ -37,27 +37,27 @@ import Button from '@/components/Button.vue';
   components: {Button, FormItem, Labels},
 })
 export default class EditLabel extends Vue {
-   get tag(){
+   get currentTag(){
      return this.$store.state.currentTag;
    }
   created() {
     const id = this.$route.params.id;
     this.$store.commit('fetchTags');
     this.$store.commit('setCurrentTag',id)
-    if (!this.tag) {
+    if (!this.currentTag) {
       this.$router.replace('/404');
     }
   }
 
   update(name: string) {
-    if (this.tag) {
-      this.$store.commit('updateTag',{id:this.tag.id,name})
+    if (this.currentTag) {
+      this.$store.commit('updateTag',{id:this.currentTag.id,name})
     }
   }
 
   remove() {
-     if (this.tag) {
-        this.$store.commit('removeTag',this.tag.id)
+     if (this.currentTag) {
+        this.$store.commit('removeTag',this.currentTag.id)
      }
   }
 
