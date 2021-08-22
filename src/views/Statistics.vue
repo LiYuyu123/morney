8665660800a44f827696598d09c47ef6d4b33aad
LiYu <2248868140@qq.com>
2021-08-22 17:00:30 +0800

    基本完成chart

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout>
     <div class="header">
-      <div >统计</div>
+      <div>统计</div>
     </div>
     <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
     <div class="charts-wrapper" ref="chartWrapper">
@@ -10,12 +10,12 @@
     <div>
       <ol v-if="groupedList.length>0">
         <li v-for="(group,index) in groupedList" :key="index">
-          <h3 class="title">{{ beautify(group.title) }}<span>￥{{group.total}}</span></h3>
+          <h3 class="title">{{ beautify(group.title) }}<span>￥{{ group.total }}</span></h3>
           <ol>
             <li v-for="(item,index) in group.items" :key="index"
                 class="record"
             >
-              <span>{{tagArray(item.tags)}}</span>
+              <span>{{ tagArray(item.tags) }}</span>
               <span class="notes">{{ item.notes }}</span>
               <span>￥{{ item.amount }}</span>
             </li>
@@ -37,15 +37,15 @@ import recordTypeList from '@/constants/recordTypeList';
 import dayjs from 'dayjs';
 import clone from '@/lib/Clone';
 import Charts from '@/components/Charts.vue';
-import _ from'lodash'
+import _ from 'lodash';
 
 @Component({
-  components: {Tabs,Charts}
+  components: {Tabs, Charts}
 
 })
 export default class Statistics extends Vue {
   tagArray(tags: Tag[]) {
-    return tags.length === 0 ? '无' : tags.map(i=>i.name).join('，');
+    return tags.length === 0 ? '无' : tags.map(i => i.name).join('，');
   }
 
   get recordList() {
@@ -54,8 +54,8 @@ export default class Statistics extends Vue {
 
   get groupedList() {
     const {recordList} = this;
-    type Result= { title: string, total?: number, items: RecordItem[] }[]
-    const newList = clone(recordList).filter(r=>r.type===this.type).sort((a, b) => dayjs(b.createAt).valueOf() - dayjs(a.createAt).valueOf());
+    type Result = { title: string, total?: number, items: RecordItem[] }[]
+    const newList = clone(recordList).filter(r => r.type === this.type).sort((a, b) => dayjs(b.createAt).valueOf() - dayjs(a.createAt).valueOf());
     if (newList.length === 0) {
       return [] as Result;
     }
@@ -70,69 +70,74 @@ export default class Statistics extends Vue {
       }
     }
 
-    result.map(group=>{
-      group.total=group.items.reduce((sum,item)=> sum+item.amount,0)
-    })
+    result.map(group => {
+      group.total = group.items.reduce((sum, item) => sum + item.amount, 0);
+    });
 
     return result;
   }
 
-  get y(){
-    const today=new Date()
-    const array=[]
-    for (let i=0;i<=29;i++){
-      const dateString=dayjs(today).subtract(i,'day').format('YYYY-MM-DD')
-      const found=_.find(this.recordList,{createAt:dateString})
-      array.push({date:dateString,value:found ? found.amount:0})
+  get y() {
+    const today = new Date();
+    const array = [];
+    for (let i = 0; i <= 29; i++) {
+      const dateString = dayjs(today).subtract(i, 'day').format('YYYY-MM-DD');
+      const found = _.find(this.recordList, {createAt: dateString});
+      array.push({key: dateString, value: found ? found.amount : 0});
     }
-    array.sort((a,b)=>{
-      if(a.date>b.date){
-        return 1
-      }else  if(a.date===b.date){
-        return  0
-      }else if(a.date<b.date){
-        return -1
+    array.sort((a, b) => {
+      if (a.key > b.key) {
+        return 1;
+      } else if (a.key === b.key) {
+        return 0;
+      } else{
+        return -1;
       }
-    })
-    return array
+    });
+    return array;
   }
 
-  get x(){
+  get x() {
 
-    const keys=this.y.map(i=>i.date)
-    const dates=this.y.map(i=>i.value)
+    const keys = this.y.map(i => i.key);
+    const dates = this.y.map(i => i.value);
     return {
-      grid:{
-        left:0,
-        right:0
+      grid: {
+        left: 0,
+        right: 0
       },
       xAxis: {
         type: 'category',
-        data:keys,
-        axisTick:{
-          show:false,
+        data: keys,
+        axisTick: {
+          show: false,
         },
-        axisLine:{
-          lineStyle:{color:'#666'}
+        axisLine: {
+          lineStyle: {color: '#666'}
+        },
+        axisLabel:{
+          formatter: function (value:string, index:number) {
+            return value.substr(5);
+          }
         }
       },
       yAxis: {
         type: 'value',
-        show:false
+        show: false
       },
-      tooltip:{
-        show:true,
+      tooltip: {
+        show: true,
         formatter: '{c}',
-        position:'top'
+        position: 'top'
       },
       series: [{
-        symbol:'circle',
-        itemStyle:{
-          color:'#666'
+        symbol: 'circle',
+        itemStyle: {
+          color: '#666'
         },
         data: dates,
         type: 'line',
-         symbolSize:12
+        symbolSize: 12
       }]
     };
   }
@@ -153,29 +158,33 @@ export default class Statistics extends Vue {
     }
   }
 
-  mounted(){
-    const div=(this.$refs.chartWrapper as HTMLDivElement)
-        div.scrollLeft=div.scrollWidth
+  mounted() {
+    const div = (this.$refs.chartWrapper as HTMLDivElement);
+    div.scrollLeft = div.scrollWidth;
   }
 
   beforeCreate() {
     this.$store.commit('fetchRecords');
   }
+
   type = '-';
   recordTypeList = recordTypeList;
 }
 </script>
 <style lang="scss" scoped>
-.noResult{
+.noResult {
   padding: 16px;
   text-align: center;
 }
+
 ::v-deep {
 
   .type-tabs-item {
-    background:rgb(232, 232, 232);
+    background: rgb(232, 232, 232);
+
     &.selected {
       background: white;
+
       &::after {
         display: none;
       }
@@ -191,6 +200,7 @@ export default class Statistics extends Vue {
 </style>
 <style lang="scss" scoped>
 @import "~@/assets/style/helper.scss";
+
 %item {
   padding: 8px 16px;
   line-height: 24px;
@@ -198,9 +208,11 @@ export default class Statistics extends Vue {
   justify-content: space-between;
   align-items: center;
 }
+
 .header {
   @extend %outerShadow;
   background: #c4c4c4;
+
   div {
     height: 50px;
     display: flex;
@@ -209,6 +221,7 @@ export default class Statistics extends Vue {
     font-size: 24px;
   }
 }
+
 .title {
   @extend %item
 }
@@ -224,11 +237,14 @@ export default class Statistics extends Vue {
   color: #999;
   overflow: auto;
 }
-.charts{
+
+.charts {
   width: 430%;
-  &-wrapper{
+
+  &-wrapper {
     overflow: auto;
-    &::-webkit-scrollbar{
+
+    &::-webkit-scrollbar {
       display: none;
     }
   }
